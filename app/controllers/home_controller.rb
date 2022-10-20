3bb3e30a5c3de3f78954c6bd056343a7e5dc22b6
SHOBRIO <030a.orz@gmail.com>
2022-10-20 16:28:04 +0900

    講師からのアドバイスをコメアウトで追記

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,15 +2,25 @@
 class HomeController < ApplicationController
   def index
     @plans = Plan.all
+    @select_plan_price
+    #createで受け取ったパラメーターをもとに表示する
   end
 
   def create
-    select_plan = Plan.where(id: params[:plans])
-    select_plan.each { |plan|
-      result = 0
-      result += plan.price
-    }
-    binding.pry
-    redirect_to root_path, sucsess: "計算しました"
+    @select_plan = Plan.where(id: params[:plans])
+    @select_plan_price = select_plan_price
+    #パラメーターをindexに渡すことで引き継げる
+    #redirect_to_root_path
   end
+end
+
+
+private
+
+def select_plan_price
+  price = @select_plan.map { |plan|
+  plan.price
+  }
+  binding.pry
+  price.sum
 end
